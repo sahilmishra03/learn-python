fd533d180ee8a8fa228f0ac07380a584aa242aef
Sahil Mishra <106021283+sahilmishra03@users.noreply.github.com>
2026-03-07 20:00:48 +0530

    Officially python is now completed basics part now moving towards the backend part ✅

--- 06_oops/01_solution.py
+++ 06_oops/01_solution.py
@@ -1,68 +1,343 @@
-class Car:
-    car_count=0
-    def __init__(self):
-        Car.car_count=Car.car_count+1
-    def set_brand(self,brand):
-        self.__brand=brand
-    def set_fuelTyepe(self,fuelType):
-        self.__fuelType=fuelType
-    def nameOfCar(self):
-        print(self.__brand+" "+self.model) 
-    def get_brand(self):
-        return self.__brand
-    def fuelTyepe(self):
-        print("Petrol")
-
-    @staticmethod
-    def carDescription():
-        return "Car is used for travelling"
-
-# class ElectricCar(Car):
-#     def __init__(self,brand,model,batterySize):
+# class Car:
+#     def __init__(self,brand,model):
 #         self.brand=brand
+#         self.model=model
+
+# my_car = Car("Tata","Safari")
+# print(my_car.brand)
+# print(my_car.model)
+
+# class Car:
+#     def __init__(self,brand,model):
+#         self.brand=brand
+#         self.model=model
+#     def fullName(self):
+#         return self.brand+"-"+self.model
+
+# my_car = Car("Tata","Safari")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.fullName())
+
+# class Car:
+#     def __init__(self,brand,model):
+#         self.brand=brand
+#         self.model=model
+#     def fullName(self):
+#         return self.brand+"-"+self.model
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model,battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size=battery_size
+
+# my_electric_car=ElectricCar("Tesla","S Model","85kWh")
+# print(my_electric_car.brand)
+# print(my_electric_car.model)
+# print(my_electric_car.battery_size)
+# print(my_electric_car.fullName())
+
+# my_car = Car("Tata","Safari")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.fullName())
+
+# class Car:
+#     def __init__(self,brand,model):
+#         self.__brand=brand
+#         self.model=model
+#     def fullName(self):
+#         return self.brand+"-"+self.model
+#     def get_brand(self):
+#         return self.__brand
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model,battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size=battery_size
+
+# # my_electric_car=ElectricCar("Tesla","S Model","85kWh")
+# # print(my_electric_car.brand)
+# # print(my_electric_car.model)
+# # print(my_electric_car.battery_size)
+# # print(my_electric_car.fullName())
+
+# my_car = Car("Tata","Safari")
+# # print(my_car.brand)
+# print(my_car.model)
+# print(my_car.get_brand())
+
+# class Car:
+#     def __init__(self,model):
+#         self.model=model
+#     def fullName(self):
+#         return self.brand+"-"+self.model
+#     def get_brand(self):
+#         return self.__brand
+#     def set_brand(self,brand):
+#         self.__brand=brand
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model,battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size=battery_size
+
+# # my_electric_car=ElectricCar("Tesla","S Model","85kWh")
+# # print(my_electric_car.brand)
+# # print(my_electric_car.model)
+# # print(my_electric_car.battery_size)
+# # print(my_electric_car.fullName())
+
+# my_car = Car("S Model")
+# # print(my_car.brand)
+# print(my_car.model)
+# my_car.set_brand("Tesla")
+# print(my_car.get_brand())
+
+# class Car:
+#     def __init__(self,brand,model):
+#         self.__brand=brand
+#         self.model=model
+#     def fullName(self):
+#         return self.brand+"-"+self.model
+#     def get_brand(self):
+#         return self.__brand
+#     def fuelType(self):
+#         return "Petrol-Diease"
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model,battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size=battery_size
+#     def fuelType(self):
+#         return "Electic"
+
+# my_electric_car=ElectricCar("Tesla","S Model","85kWh")
+# # print(my_electric_car.brand)
+# print(my_electric_car.model)
+# print(my_electric_car.battery_size)
+# # print(my_electric_car.fullName())
+# print(my_electric_car.fuelType())
+
+
+# my_car = Car("Tata","Safari")
+# # print(my_car.brand)
+# print(my_car.model)
+# print(my_car.get_brand())
+# print(my_car.fuelType())
+
+# class Car:
+#     total_car=0
+#     def __init__(self,brand,model):
+#         Car.total_car=Car.total_car+1
+#         self.__brand=brand
+#         self.model=model
+#     def fullName(self):
+#         return self.brand+"-"+self.model
+#     def get_brand(self):
+#         return self.__brand
+#     def fuelType(self):
+#         return "Petrol-Diease"
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model,battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size=battery_size
+#     def fuelType(self):
+#         return "Electic"
+
+# my_electric_car=ElectricCar("Tesla","S Model","85kWh")
+# # print(my_electric_car.brand)
+# print(my_electric_car.model)
+# print(my_electric_car.battery_size)
+# # print(my_electric_car.fullName())
+# print(my_electric_car.fuelType())
+
+
+# my_car = Car("Tata","Safari")
+# # print(my_car.brand)
+# print(my_car.model)
+# print(my_car.get_brand())
+# print(my_car.fuelType())
+# print(my_car.total_car)
+
+# my_car2 = Car("Tata","Nexon")
+# # print(my_car.brand)
+# print(my_car2.model)
+# print(my_car2.get_brand())
+# print(my_car2.fuelType())
+# print(Car.total_car)
+
+# class Car:
+#     total_car=0
+#     def __init__(self,brand,model):
+#         Car.total_car=Car.total_car+1
+#         self.__brand=brand
+#         self.model=model
+#     def fullName(self):
+#         return self.brand+"-"+self.model
+#     def get_brand(self):
+#         return self.__brand
+#     def fuelType(self):
+#         return "Petrol-Diease"
+#     @staticmethod
+#     def general_desc():
+#         return "Car Class"
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model,battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size=battery_size
+#     def fuelType(self):
+#         return "Electic"
+#     @staticmethod
+#     def general_desc():
+#         return "Electric Car Class"
+
+# my_electric_car=ElectricCar("Tesla","S Model","85kWh")
+# # print(my_electric_car.brand)
+# print(my_electric_car.model)
+# print(my_electric_car.battery_size)
+# # print(my_electric_car.fullName())
+# print(my_electric_car.fuelType())
+
+
+# my_car = Car("Tata","Safari")
+# # print(my_car.brand)
+# print(my_car.model)
+# print(my_car.get_brand())
+# print(my_car.fuelType())
+# print(my_car.total_car)
+
+# my_car2 = Car("Tata","Nexon")
+# # print(my_car.brand)
+# print(my_car2.model)
+# print(my_car2.get_brand())
+# print(my_car2.fuelType())
+# print(Car.general_desc())
+
+# class Car:
+#     total_car=0
+#     def __init__(self,brand,model):
+#         Car.total_car=Car.total_car+1
+#         self.__brand=brand
 #         self.__model=model
-#         self.batterySize=batterySize
-#     def fuelTyepe(self):
-#         print("Electric")
+#     def fullName(self):
+#         return self.__brand+"-"+self.__model
+#     def get_brand(self):
+#         return self.__brand
+#     def fuelType(self):
+#         return "Petrol-Diease"
+#     @staticmethod
+#     def general_desc():
+#         return "Car Class"
+    
 #     @property
 #     def model(self):
 #         return self.__model
-        
-# tesla = ElectricCar("Tesla","S","85kwh")
-# print(isinstance(tesla,Car))
-# print(isinstance(tesla,ElectricCar))
-# tesla.model="Nexon"
-# print(tesla.model)
-# tesla.fuelTyepe()
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model,battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size=battery_size
+#     def fuelType(self):
+#         return "Electic"
+#     @staticmethod
+#     def general_desc():
+#         return "Electric Car Class"
 
-# safari = Car()
-# safari.model="Nexon"
-# print(safari.model)
-# newCar = Car("Hyundai","Creata")
-# print(newCar.model)
-# print(newCar.brand)
-# newCar.nameOfCar()
+# my_electric_car=ElectricCar("Tesla","S Model","85kWh")
+# print(my_electric_car.brand)
+# print(my_electric_car.model)
+# print(my_electric_car.battery_size)
+# print(my_electric_car.fullName())
+# print(my_electric_car.fuelType())
 
-# newCar2 = Car("Toyota","Fortuner")
-# print(newCar2.model)
-# print(newCar2.brand)
-# newCar2.nameOfCar()
 
-class Car:
-    def __init__(self):
-        print("This is a Car class")
+# my_car = Car("Tata","Safari")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.get_brand())
+# print(my_car.fuelType())
+# print(my_car.total_car)
+
+# my_car2 = Car("Tata","Nexon")
+# my_car2.model="City"
+# print(my_car.brand)
+# print(my_car2.model)
+# print(my_car2.get_brand())
+# print(my_car2.fuelType())
+# print(Car.general_desc())
+
+# class Car:
+#     total_car=0
+#     def __init__(self,brand,model):
+#         Car.total_car=Car.total_car+1
+#         self.__brand=brand
+#         self.__model=model
+#     def fullName(self):
+#         return self.__brand+"-"+self.__model
+#     def get_brand(self):
+#         return self.__brand
+#     def fuelType(self):
+#         return "Petrol-Diease"
+#     @staticmethod
+#     def general_desc():
+#         return "Car Class"
+    
+#     @property
+#     def model(self):
+#         return self.__model
+    
+# class ElectricCar(Car):
+#     def __init__(self, brand, model,battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size=battery_size
+#     def fuelType(self):
+#         return "Electic"
+#     @staticmethod
+#     def general_desc():
+#         return "Electric Car Class"
+
+# my_electric_car=ElectricCar("Tesla","S Model","85kWh")
+# # print(my_electric_car.brand)
+# print(my_electric_car.model)
+# print(my_electric_car.battery_size)
+# print(my_electric_car.fullName())
+# print(my_electric_car.fuelType())
+
+
+# my_car = Car("Tata","Safari")
+# # print(my_car.brand)
+# print(my_car.model)
+# print(my_car.get_brand())
+# print(my_car.fuelType())
+# print(my_car.total_car)
+
+# my_car2 = Car("Tata","Nexon")
+# # my_car2.model="City"
+# # print(my_car.brand)
+# print(my_car2.model)
+# print(my_car2.get_brand())
+# print(my_car2.fuelType())
+# print(Car.general_desc())
+
+# print(isinstance(my_car2,Car))
+# print(isinstance(my_car,Car))
+# print(isinstance(my_electric_car,Car))
+# print(isinstance(my_electric_car,ElectricCar))
 
 class Battery:
-    def __init__(self):
-        print("This is a Battery class")
+    def battery_info(self):
+        print("Lithium Ions Battery")
 
 class Engine:
-    def __init__(self):
-        print("This is a Engine class")
+    def engine_info(self):
+        print("500 HORSE POWER ENGINE")
 
-class ElectricCar(Battery, Engine, Car):
-    def __init__(self):
-        super().__init__()
-        print("This is a Electric Car class")
+class ElectricCar(Battery,Engine):
+    pass
 
-tesla = ElectricCar()
+my_new_Electric_Car=ElectricCar()
+my_new_Electric_Car.battery_info()
+my_new_Electric_Car.engine_info()
--- README.md
+++ README.md
@@ -1,57 +1,18 @@
-# 🐍 Learn Python -- Backend Journey
+# 🐍 Python Learning
 
-This repository contains my Python learning journey focused on backend
-development.\
-I am learning Python to build scalable backend systems and APIs.
+This repository is for learning Python.
 
-------------------------------------------------------------------------
+I have completed the Python basics.  
+You can check the folder names to see the topics covered.
 
-## 🚀 Goals
+## 🐍 Topics Completed
 
--   Learn Python fundamentals\
--   Understand OOP concepts\
--   Practice problem solving\
--   Learn backend frameworks (Flask / FastAPI / Django)\
--   Build REST APIs\
--   Connect backend with databases
+- Variables & Data Types
+- Conditionals
+- Loops
+- Functions
+- Scopes
+- OOP (Classes & Objects)
+- Decorators
 
-------------------------------------------------------------------------
-
-## 📚 Topics Covered
-
--   ✅ Variables & Data Types\
--   ✅ Conditionals & Loops\
--   ✅ Functions\
--   ✅ OOP (Classes & Objects)\
--   ✅ Exception Handling\
--   ✅ File Handling\
--   ✅ Modules & Packages\
-
-------------------------------------------------------------------------
-
-## 🛠️ Tech Stack (Learning)
-
--   Python\
--   Git & GitHub\
--   VS Code
-
-------------------------------------------------------------------------
-
-## 🎯 Future Plan
-
--   Build REST APIs\
--   Connect with PostgreSQL / MongoDB\
--   Integrate with Flutter frontend\
--   Deploy backend project
-
-------------------------------------------------------------------------
-
-## 💡 Why This Repo?
-
-This repository tracks my daily progress while learning Python for
-backend development.\
-I will keep updating it as I learn new concepts and build projects.
-
-------------------------------------------------------------------------
-
-⭐ If you're also learning Python, feel free to fork or connect!
+All basic Python concepts are completed.
